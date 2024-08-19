9e01c707db853138504b912fa09f7ceef0b7985b
Monika <laiskutis.monikai@gmail.com>
2024-08-19 23:13:55 +0300

    statistic

--- src/components/Statistic.jsx
+++ src/components/Statistic.jsx
@@ -1,15 +1,16 @@
-import { useState } from "react";
+export default function Statistic(){  
 
-export function Statistic(){  
-    
-    
-    
+  const score1 = localStorage.getItem("loseData");
+  const lose = JSON.parse(score1);
 
-    return (
+  const score2 = localStorage.getItem("winData");
+  const win = JSON.parse(score2);
+    
+  return (
         <>
         <div className="count">
-            <p className="sum">Win sum: 0</p>
-            <p className="sum">Lose sum: 0</p>
+            <p className="sum">Win sum: {win}</p>
+            <p className="sum">Lose sum: {lose}</p>
         </div>     
         </>
     );
